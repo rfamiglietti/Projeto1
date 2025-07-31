d6ad9f0fe5b6d0fea31786faa8be906ae09369a2
rfamiglietti <romulopfami@gmail.com>
2025-07-30 21:44:25 -0300

    aula 2 completa

--- recipes/models.py
+++ recipes/models.py
@@ -4,7 +4,7 @@ class Recipe(models.Model):
     title = models.CharField(max_length=200)
     ingredients = models.TextField()
     intrucions = models.TextField()
-    cooking_time = models.ImageField(help_text='in minutes')
+    cooking_time = models.IntegerField(help_text='in minutes')
     
     def __str__(self):
         return self.title
