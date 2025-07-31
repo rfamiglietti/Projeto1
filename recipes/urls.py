cf489fadc62330f0cf839458a0a3bb84415e4d3a
rfamiglietti <romulopfami@gmail.com>
2025-07-31 19:52:50 -0300

    aula 3

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='recipes_home'),
+    path('', views.recipe_list, name='recipes_list'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Recipe
+# recipes/views.py
 
-def home(request):
-    return HttpResponse("Bem-vindo à página de receitas!")
+def recipe_list(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'recipes/recipe_list.html', {'recipes': recipes})
+
+def recipe_detail(request, pk):
+    recipe = get_object_or_404(Recipe, pk=pk)
+    return render(request, 'recipes/recipe_detail.html', {'recipe': recipe})
